b80c0308ad4de9aae76c246f36b8d111a6bbbbfb
Strayneko <bayurendi10@gmail.com>
2024-07-07 16:19:37 +0700

    Update: response type

--- types/response.types.go
+++ types/response.types.go
@@ -1,8 +1,13 @@
 package types
 
 type ResponseType struct {
-	Status  bool         `json:"status"`
-	Code    int          `json:"code"`
-	Message string       `json:"message"`
-	Data    *[]ComicType `json:"data"`
+	Status      bool         `json:"status"`
+	Code        int16        `json:"code"`
+	Message     string       `json:"message"`
+	Total       int16        `json:"total,omitempty"`
+	CurrentPage int16        `json:"current_page,omitempty"`
+	PrevPage    int16        `json:"prev_page,omitempty"`
+	NextPage    int16        `json:"next_page,omitempty"`
+	LastPage    int16        `json:"last_page,omitempty"`
+	Data        *[]ComicType `json:"data"`
 }
