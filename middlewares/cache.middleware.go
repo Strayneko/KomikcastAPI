184d133580f6d1fe4289b2be9296aaf83d44c965
Strayneko <bayurendi10@gmail.com>
2024-07-09 20:18:34 +0700

    Feat: add type to middleware cache

--- middlewares/cache.middleware.go
+++ middlewares/cache.middleware.go
@@ -2,22 +2,31 @@ package middlewares
 
 import (
 	"encoding/json"
+	"net/http"
+	"time"
+
 	"github.com/Strayneko/KomikcastAPI/configs"
 	"github.com/Strayneko/KomikcastAPI/types"
 	"github.com/gofiber/fiber/v2"
-	"net/http"
-	"time"
 )
 
-func CacheMiddleware(param string, defaultVal string) fiber.Handler {
+// CacheMiddleware creates a middleware for caching GET requests based on page type.
+// It caches responses for 60 minutes, using the request path and query parameters to generate cache keys.
+func CacheMiddleware(pageType string) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
-		key := "?" + param + ctx.Query(param, defaultVal)
 		if ctx.Method() != "GET" {
 			// Only cache GET requests
 			return ctx.Next()
 		}
+		params := "?"
+		page := ctx.Query("page", "1")
+		if pageType == "list" {
+			params += "page=" + page
+		} else if pageType == "search" {
+			params += "query=" + ctx.Query("query", "") + "&page=" + page
+		}
 
-		cacheKey := ctx.Path() + key // Generate a cache key from the request path and query parameters
+		cacheKey := ctx.Path() + params // Generate a cache key from the request path and query parameters
 		// Check if the response is already in the cache
 		if cached, found := configs.Cache.Get(cacheKey); found {
 			return ctx.JSON(cached)
@@ -34,7 +43,7 @@ func CacheMiddleware(param string, defaultVal string) fiber.Handler {
 			return ctx.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 		}
 
-		// Cache the response for 10 minutes
+		// Cache the response for 60 minutes
 		configs.Cache.Set(cacheKey, data, 60*time.Minute)
 		return nil
 	}
