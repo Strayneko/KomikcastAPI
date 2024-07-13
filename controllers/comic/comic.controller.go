d4cc28d8a2386b2d466ce8b967ca82e1bc03e617
Strayneko <bayurendi10@gmail.com>
2024-07-13 22:35:24 +0700

    Feat: add orderby query params

--- controllers/comic/comic.controller.go
+++ controllers/comic/comic.controller.go
@@ -6,7 +6,10 @@ import (
 	"github.com/Strayneko/KomikcastAPI/interfaces"
 	"github.com/Strayneko/KomikcastAPI/services/comic"
 	"github.com/gofiber/fiber/v2"
+	"net/http"
+	"slices"
 	"strconv"
+	"strings"
 )
 
 var Helper interfaces.Helper
@@ -26,6 +29,11 @@ func (h *handler) GetComicList(ctx *fiber.Ctx) error {
 	path := "manga/"
 	orderBy := ctx.Query("order")
 
+	if !slices.Contains(configs.ComicOrderParams, orderBy) {
+		orderTypes := strings.Join(configs.ComicOrderParams, ",")
+		return Helper.ResponseError(ctx, fiber.NewError(http.StatusBadRequest, "Order should be in "+orderTypes))
+	}
+
 	currentPage, err := Helper.ValidatePage(ctx)
 	if err != nil {
 		return Helper.ResponseError(ctx, err)
@@ -35,7 +43,7 @@ func (h *handler) GetComicList(ctx *fiber.Ctx) error {
 		path += "?page=" + strconv.Itoa(int(currentPage))
 	}
 	if len(orderBy) > 0 {
-		path += "&order=" + configs.GetComicOrderBy().Popular
+		path += "&order=" + configs.GetComicOrderBy(orderBy)
 	}
 
 	return ComicService.GetComicList(ctx, path, currentPage)
