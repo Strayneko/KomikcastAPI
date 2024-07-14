0d23e9a2566168e94be170763d3dbce93909ccae
Strayneko <bayurendi10@gmail.com>
2024-07-14 13:29:12 +0700

    Feat: handle redis connection error

--- configs/cache.config.go
+++ configs/cache.config.go
@@ -1,14 +1,22 @@
 package configs
 
 import (
+	"context"
+	"fmt"
 	"github.com/redis/go-redis/v9"
 )
 
 var Cache *redis.Client
 
 func InitCache() {
+	host := ViperEnv.GetString("REDIS_HOST")
+	port := ViperEnv.GetString("REDIS_PORT")
 	Cache = redis.NewClient(&redis.Options{
-		Addr: ViperEnv.Get("REDIS_HOST").(string) + ":" + ViperEnv.Get("REDIS_PORT").(string),
+		Addr: fmt.Sprintf("%s:%s", host, port),
 		DB:   0,
 	})
+
+	if err := Cache.Ping(context.Background()).Err(); err != nil {
+		panic(err)
+	}
 }
